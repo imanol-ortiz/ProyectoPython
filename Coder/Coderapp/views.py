d8a55dfacd7c203933cfb5e8b704702f8061bf80
imanolortiz@outlook.es <imanolortiz@outlook.es>
2022-05-31 21:45:18 -0300

    implementando forms

--- Coder/Coderapp/views.py
+++ Coder/Coderapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render, HttpResponse
 
+from Coderapp.forms import*
+
 # Create your views here.
 
 def Home(request):
@@ -8,12 +10,51 @@ def Home(request):
     
 def Producto(request):
 
-        return render(request,"Coderapp/producto.html")
+        if request.method=='POST':
+                miformulario=ProductoFormulario(request.Post)
+                print(miformulario)
+
+                if miformulario.is_valid:
+                        informacion=miformulario.cleaned_data
+                        producto=Producto(nombre=['nombre'],stock=['stock'])
+                        producto.save
+                        
+                        return render(request,"Coderapp/producto.html")
+        else:
+                miformulario=ProductoFormulario()
+        
+        return render(request,"Coderapp/producto.html", {"miformulario":miformulario})
     
 def Vendedor(request):
 
-        return render(request,"Coderapp/vendedor.html")
+        if request.method=='POST':
+                miformulario=VendedorFormulario(request.Post)
+                print(miformulario)
+
+                if miformulario.is_valid:
+                        informacion=miformulario.cleaned_data
+                        vendedor=Vendedor(nombre=['nombre'],apellido=['apellido'],telefono=['telefono'])
+                        vendedor.save
+                        
+                        return render(request,"Coderapp/vendedor.html")
+        else:
+                miformulario=VendedorFormulario()
+        
+        return render(request,"Coderapp/vendedor.html", {"miformulario":miformulario})
     
 def Sucursal(request):
 
-        return render(request,"Coderapp/sucursal.html")
+        if request.method=='POST':
+                miformulario=SucursalFormulario(request.Post)
+                print(miformulario)
+
+                if miformulario.is_valid:
+                        informacion=miformulario.cleaned_data
+                        sucursal=Sucursal(numero=['numero'],direccion=['direccion'])
+                        sucursal.save
+                        
+                        return render(request,"Coderapp/sucursal.html")
+        else:
+                miformulario=SucursalFormulario()
+        
+        return render(request,"Coderapp/sucursal.html", {"miformulario":miformulario})
